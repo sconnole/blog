97dbb7b175eaa03cb1d2d95364144bd201545f0d
Sean Connole <sean.connole@dmetrics.com>
2025-07-13 12:08:41 -0400

    feat: scaffolding for home and posts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home.component';
+import { PostDetailComponent } from './pages/post-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'posts/:slug', component: PostDetailComponent },
+];
---
+++ src/assets/posts/hello-world.md
@@ -0,0 +1,6 @@
+# Hello World
+
+Welcome to my Angular blog powered by markdown!
+
+- This is rendered from a file
+- Styled with Tailwind
